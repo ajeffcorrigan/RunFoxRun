331355be49601d8f212de054e803514b7bb2ff9b
ajeffcorrigan <ajeffcorrigan@yahoo.com>
2015-01-21 20:14:57 -0400

    Initial commit

---
+++ desktop/src/com/ajeffcorrigan/runfoxrun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ajeffcorrigan.runfoxrun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ajeffcorrigan.runfoxrun.RunFoxRun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RunFoxRun(), config);
+	}
+}
---
+++ html/src/com/ajeffcorrigan/runfoxrun/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.ajeffcorrigan.runfoxrun.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.ajeffcorrigan.runfoxrun.RunFoxRun;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new RunFoxRun();
+        }
+}
